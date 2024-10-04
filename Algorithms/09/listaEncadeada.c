c8736e7e7a0e2b01947467cced101555f5a68a0f
Cabr1to <luis.b.c.melo@gmail.com>
2024-10-04 10:53:08 -0300

    add funcao libera

--- Algorithms/09/listaEncadeada.c
+++ Algorithms/09/listaEncadeada.c
@@ -19,31 +19,77 @@ int main()
 {
     // cria a cabeca da lista
     Item head;
-    head.prox == NULL;
+    head.prox = NULL;
 
     printf("sizeof (Item) = %d\n", sizeof(Item));
 
     // Inserindo dados...
 
     printf("\nInserindo itens novos...\n");
+    insereFim(&head, 30);
     insereFim(&head, 19);
-    insereFim(&head, 10);
-    insereFim(&head, 50);
+    insereFim(&head, 134);
+
+    system("PAUSE");
+
+    // Imprimir os itens da lista
+    printf("\nImprimindo os itens\n");
+    imprime(&head);
+
+    system("PAUSE");
+
+    // Libera memoria
+    printf("\nLIberando memoria dos itens\n");
+    libera(&head);
+    return 0;
 }
 
 void imprime(Item *head)
 {
-    if (head != NULL)
+    Item *atual = head->prox;
+
+    if (atual != NULL)
     {
-        printf("%d\n", head->valor);
-        imprimir(head->prox);
+        printf("%d\n", atual->valor);
+        imprime(head->prox);     
     }
 }
 
 void insereFim(Item *head, int valor)
 {
-    if (head != NULL){
-        head->prox = valor;
+    // Cria um ponteiro para o novo item e aloca dinamicamente
+    Item *novo = (Item*)malloc(sizeof(Item));
+
+    novo->prox = NULL;
+    novo->valor = valor;
+
+    printf("Item de valor %3d alocado no endereco: %x\n", novo->valor, novo);
+
+    // varre a lista do comeco para chegar na ultima posicao
+    Item *atual = head;
+    while (atual->prox != NULL)
+    {
+        atual = atual->prox;
     }
-    
+
+    // chegando na ultima posicao adiciona o novo elemento
+    atual->prox = novo;
+}
+
+void libera(Item *head)
+{
+    // ponteiro para o inicio da lista
+    Item *atual = head->prox;
+
+    Item *liberado;
+
+    while (atual != NULL)
+    {
+        liberado = atual;
+
+        atual = atual->prox;
+        printf("Liberando o item de valor %3d na posicao %x\n",liberado->valor, liberado);
+
+        free(liberado);
+    }
 }
